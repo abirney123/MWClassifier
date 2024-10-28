eb12695729dab550d9d52ec8be2cdc74a770b641
oawajan <omar.awajan@uvm.edu>
2024-10-28 13:12:25 -0400

    1- Duplication is added to handle imbalanced data 2- add multi 1-D conv layers

--- CNN.py
+++ CNN.py
@@ -11,8 +11,19 @@ from torchmetrics import F1Score, AUROC
 import torch.nn.functional as F
 from torch.optim import Adam,AdamW
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, LeaveOneGroupOut
 import torch
 from torchmetrics.classification import BinaryAccuracy, BinaryAUROC, BinaryPrecisionRecallCurve, BinaryConfusionMatrix, BinaryF1Score, BinaryMatthewsCorrCoef, BinaryPrecision, BinaryRecall
+from collections import defaultdict, Counter
+
+
+
+def class_counts(dataset: torch.utils.data.Dataset) -> dict:
+    labels = defaultdict(int)
+    for i in range(len(dataset)):
+        _, label = dataset[i]
+        labels[label.item()] += 1
+    return dict(labels)
 
 def combine_records(df :pd.DataFrame) ->pd.DataFrame:
     # written by Omar Awajan, adapted by Alaina Birney
@@ -40,23 +51,23 @@ def combine_records(df :pd.DataFrame) ->pd.DataFrame:
  
 # this is how I changed it. Sure thing let me just grab my headphones
 
-def get_label(df :pd.DataFrame) -> float:
+def get_label(df :pd.DataFrame) -> int:
     counts = df['is_MW'].value_counts()
     if (len(counts.keys()) == 1):
         return counts.keys()[0]
     ratio = counts.iloc[1]/counts.iloc[0]
-    if (ratio > 0.50) & (counts.keys()[0] == 1):
+    if (ratio > 0.10) & (counts.keys()[0] == 1):
         return 1
     return 0
 
 
-def sliding_window(df :pd.DataFrame, window_size :int=2000, step_size :int=500):
+def sliding_window(df :pd.DataFrame, window_size :int=2500, step_size :int=250):
     start = 0
     # forward propagation to fill NaN values
     df = df.ffill()
     num_columns = df.shape[1]-1
     windows :list[pd.DataFrame]= []
-    labels :list[float]= []
+    labels :list[int]= []
     while start < len(df):
         end = start + window_size
         window = df.iloc[start:end]
@@ -78,11 +89,29 @@ def sliding_window(df :pd.DataFrame, window_size :int=2000, step_size :int=500):
         windows.append(window)
         start += step_size
 
-    labels = tensor(labels, dtype=torch.bool)
+    labels = tensor(labels, dtype=torch.int)
     windows = windows
 
     return windows,labels
 
+def duplicate(dataset) -> torch.utils.data.Dataset:
+    """Function to balance dataset by duplicating minority class instances."""
+    counts = class_counts(dataset)
+    max_count = max(counts.values())
+    new_features = []
+    new_labels = []
+    for i in range(len(dataset)):
+        feature, label = dataset[i]
+        new_features.append(feature)
+        new_labels.append(label)
+        num_duplicates = max_count - counts[label.item()]
+        if num_duplicates > 0:
+            for _ in range(num_duplicates):
+                new_features.append(feature.clone())
+                new_labels.append(label.clone())
+    new_features = torch.stack(new_features)
+    new_labels = torch.tensor(new_labels)
+    return MWDataSet(features=new_features, labels=new_labels)
 
 class MWDataSet(Dataset):
     def __init__(self, features, labels):
@@ -97,17 +126,23 @@ class MWDataSet(Dataset):
 
 
 class CNNModel(nn.Module):
-    def __init__(self, size=8000):
+    def __init__(self, size=17500):
         super(CNNModel, self).__init__()
 
-        # 1D Convolution layer with a single layer
-        self.conv1 = nn.Conv1d(in_channels=1, out_channels=32, kernel_size=5, padding=0, stride=5) 
+        # First 1D Convolution layer
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=32, kernel_size=5, padding=0, stride=5)
+        conv1_out_size = ((size - 5) // 5) + 1
         
-        # Final size after conv layer
-        self.final_size = ((size - 5) // 5) + 1
+        # Second 1D Convolutional layer
+        self.conv2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, padding=0, stride=2)
+        conv2_out_size = ((conv1_out_size - 5) // 2) + 1
         
+        # Third 1D Convolutional layer
+        self.conv3 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, padding=0, stride=2)
+        conv3_out_size = ((conv2_out_size - 3) // 2) + 1
+
         # Fully connected layers
-        self.fc1 = nn.Linear(32 * self.final_size, 64)
+        self.fc1 = nn.Linear(128 * conv3_out_size, 64)
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, 64)
         self.fc4 = nn.Linear(64, 32)
@@ -120,6 +155,7 @@ class CNNModel(nn.Module):
         out = F.relu(out)
         out = out.view(out.size(0), -1)  # Flatten the tensor
         #print(f"flattened input shape: {x.shape}")        
+
         # Forward pass through fully connected layers
         out = self.fc1(out)
         out = F.relu(out)
@@ -134,7 +170,7 @@ class CNNModel(nn.Module):
         return out
 
 '''
-    ['', 'tSample', 'LX', 'LY', 'LPupil', 'RX', 'RY', 'RPupil',
+    ['Unnamed: 0', 'tSample', 'LX', 'LY', 'LPupil', 'RX', 'RY', 'RPupil',
        'page_num', 'run_num', 'is_MW', 'sample_id', 'Subject',
        'tSample_normalized']
 '''
@@ -142,9 +178,11 @@ def data_loader(filepath :str):
 
     # Load dataset and drop irrelevant columns
     df :pd.DataFrame = pd.read_csv(filepath)
-    df.drop(columns=['Unnamed: 0', 'page_num', 'tSample','run_num','LPupil_normalized','RPupil_normalized', 'sample_id','tSample_normalized'], inplace=True)
-    
+    df = df.drop(columns=['Unnamed: 0', 'page_num', 'tSample', 'sample_id','tSample_normalized']) # 'run_num',
+    print(df.columns)
     #split DataSet by Subject
+    #trainSubjects, testSubjects = lstm_VC.split_data(df)
+    
     subjectList :list[str] = df['Subject'].value_counts().keys()
     totalSize = len(subjectList)
     trainSize = int(0.8 * totalSize)
@@ -152,8 +190,6 @@ def data_loader(filepath :str):
     testSubjects :pd.DataFrame = df[df['Subject'].isin(subjectList[trainSize:])]
     trainSubjects.drop(columns=['Subject'],inplace=True)
     testSubjects.drop(columns=['Subject'], inplace=True)
-    # trainSubjects = combine_records(trainSubjects)
-    # testSubjects = combine_records(testSubjects)
     print(trainSubjects.shape)
     print(testSubjects.shape)
     training_windows, training_labels = sliding_window(trainSubjects)
@@ -166,13 +202,25 @@ def CNN()->None:
     # select GPU for training and testing if avaialble
     hw_device = device("cuda" if cuda.is_available() else "cpu")
     print(f"Using device: {hw_device}")
+    
     # init model parameters
     model = CNNModel()
     model.to(hw_device)
     criterion = nn.BCEWithLogitsLoss()
-    optimizer = Adam(model.parameters(), lr=0.00001)
+    optimizer = Adam(model.parameters())
     epochs = 64
     batch = 10
+
+    # init metrics for model evaluation
+    accuracy = BinaryAccuracy()
+    auroc = BinaryAUROC()
+    precision = BinaryPrecision()
+    confusionmatrix = BinaryConfusionMatrix()
+    f1score = BinaryF1Score()
+    corr_coef = BinaryMatthewsCorrCoef()
+    precision_recallcurve = BinaryPrecisionRecallCurve()
+    recall = BinaryRecall()
+
 
     # prepare dataset for model learning
     file_path = r"Z:\Mindless Reading\neuralnet_classifier\all_subjects_interpolated.csv" # "D:\\all_subjects_data_no_interpolation.csv"
@@ -180,7 +228,13 @@ def CNN()->None:
     
     train_dataset = MWDataSet(features=training_windows, labels=training_labels)
     test_dataset = MWDataSet(features=testing_windows, labels=testing_labels)
+    
+    train_dataset = duplicate(train_dataset)
+    test_dataset = duplicate(test_dataset)
 
+    print(f"Training Dataset: {class_counts(train_dataset)}")
+    print(f"Testing Dataset: {class_counts(test_dataset)}")
+    
     train_loader = DataLoader(train_dataset,batch_size=batch,shuffle=False)
     test_loader = DataLoader(test_dataset,batch_size=batch,shuffle=False)    
 
@@ -233,29 +287,29 @@ def CNN()->None:
     predicted_classes = (all_outputs >= 0.5).long().view(-1).cpu().numpy()
     true_classes = all_labels.view(-1).cpu().numpy()
     
-    accuracy = BinaryAccuracy(true_classes, predicted_classes, threshold=0.5)
-    auroc = BinaryAUROC(true_classes, predicted_classes)
-    precision = BinaryPrecision(true_classes, predicted_classes)
-    confusionmatrix = BinaryConfusionMatrix(true_classes, predicted_classes)
-    f1score = BinaryF1Score(true_classes, predicted_classes)
-    corr_coef = BinaryMatthewsCorrCoef(true_classes, predicted_classes)
-    precision_recallcurve = BinaryPrecisionRecallCurve(true_classes, predicted_classes)
-    recall = BinaryRecall(true_classes, predicted_classes)
 
-
-
-    print(f'Accuracy: {accuracy:.4f}')
-    print(f'AUROC: {auroc:.4f}')
-    print(f'Precision: {precision:.4f}')
-    print(f'Recall: {recall:.4f}')
+    # calculate model performance
+    acc = accuracy(true_classes, predicted_classes)
+    au_roc = auroc(true_classes, predicted_classes)
+    prec = precision(true_classes, predicted_classes)
+    rec = recall(true_classes, predicted_classes)
+    f1_score = f1score(true_classes, predicted_classes)
+    conf_mat = confusionmatrix(true_classes, predicted_classes)
+    corrcoef = corr_coef(true_classes, predicted_classes)
+    prec_recallcurve = precision_recallcurve(true_classes, predicted_classes)
+    
+    # print model performance results 
+    print(f'Accuracy: {acc:.4f}')
+    print(f'AUROC: {au_roc:.4f}')
+    print(f'Precision: {prec:.4f}')
+    print(f'Recall: {rec:.4f}')
     print(f'F1-Score: {f1score:.4f}')
-    print(f'confusion matrix: {confusionmatrix}')
-    print(f'correlation coff: {corr_coef}')
-    print(f'precision recall curve: {precision_recallcurve}')
+    print(f'confusion matrix: {conf_mat}')
+    print(f'correlation coff: {corrcoef}')
+    print(f'precision recall curve: {prec_recallcurve}')
 
 
 
-0
 def main()->None:
     CNN()
     
