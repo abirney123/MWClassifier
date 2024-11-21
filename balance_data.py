b35c4486bd0a571e5265b8c5bf00af9f9b49464b
abirney123 <159940343+abirney123@users.noreply.github.com>
2024-11-20 19:09:53 -0500

    new balancing strategy works

--- balance_data.py
+++ balance_data.py
@@ -66,7 +66,7 @@ def mw_over_time(dfSamples, cleaned=False):
             plt.plot(run_data.index, run_data.values)
             plt.xlabel("Normalized Time (ms from run start)")
             plt.ylabel("MW Frequency")
-            plt.title("Run {run}")
+            plt.title(f"Run {run}")
             plt.tight_layout()
         
         # save when done
@@ -88,33 +88,114 @@ def remove_latent(dfSamples, buffer=4000):
     for (subject, run), group in grouped:
         print(f"Processing subject {subject} run {run}")
         group= group.copy()
-        # keep col for flag within buffer
-        group["keep"] = 0# initialize to 0
-        mw_indices = group[group["is_MW"] == 1].index 
-        # loop through mw indices to add flag to samples within buffer of mw
-        for idx in mw_indices:
-            start = group.loc[idx, "tSample_normalized"] - buffer
-            end = group.loc[idx, "tSample_normalized"] + buffer
-
-            # set keep flag 
-            group.loc[(group["tSample_normalized"] >= start) & 
-                      (group["tSample_normalized"] <= end), "keep"] =1
-            
-        # identify the unflagged, nonmw periods
-        group["to_remove"] = (group["is_MW"] ==0) & (group["keep"] == 0)
-        group = group[~group["to_remove"]].copy()
+        # get tSample normalized for each sample where mw occurred for this sub run
+        mw_times = group.loc[group["is_MW"] == 1, "tSample_normalized"].values
+        # handle cases where there is no mw
+        if len(mw_times) == 0:
+            # don't include this sub run in the cleaned data
+            print(f"Removing subject {subject}, run {run}. No MW present.")
+            continue
+        
         cleaned_groups.append(group.reset_index(drop=True))
+        """
+        for removing based on buffer- commented out to just remove runs with no mw
+        # if there is some mw, create an indication of rows to keep-
+        # keep rows where tSample normalized is within buffer of mw_time
+        group["keep"] = group["tSample_normalized"].apply(
+            lambda x: ((mw_times - buffer <= x) & (x <= mw_times + buffer)).any()).astype(int)
+            
+        # retain rows where keep is 1 or mw is true
+        group = group[(group["is_MW"] == 1) | (group["keep"] == 1)].copy()
+        
+        # append to cleaned groups
+        cleaned_groups.append(group.reset_index(drop=True))
+        """
         
     cleaned_df = pd.concat(cleaned_groups, ignore_index=True)
     return cleaned_df
+
+def select_mw(dfSamples, buffer=4000):
+    """
+    Identifies mw periods, then selects those periods plus buffer surrounding them 
+    to include in dataset
+    """
+    # group by subject and run
+    grouped = dfSamples.groupby(["Subject", "run_num"])
+    selected_rows = []
+    # handle runs with no mw (drop)
+    for (subject, run), group in grouped:
+        print(f"Processing subject {subject} run {run}")
+        group= group.copy()
+        mw_rows = group[group["is_MW"] == 1]
+        # handle cases where there is no mw
+        if mw_rows.empty:
+            # don't include this sub run in the cleaned data
+            print(f"Removing subject {subject}, run {run}. No MW present.")
+            continue
+        # find mw periods - consecutive instances of mw, get tSample for start and end of each period
+        # plus buffer
+        mw_intervals = [] # mw periods plus buffer- will be list of tuples
+        for time in mw_rows["tSample_normalized"]:
+            mw_intervals.append((time-buffer, time+buffer))
+            
+        # merge intervals when they are too close together
+        mw_intervals.sort() # sort by start time
+        merged_intervals = []
+        current_start, current_end = mw_intervals[0]
+        
+        for start, end in mw_intervals[1:]:
+            if start <= current_end: # if next int starts before this one ends
+                # set the end to the max of last end and current end
+                current_end = max(current_end, end)
+            else:
+                # no overlap, append as normal
+                merged_intervals.append((current_start, current_end))
+                current_start, current_end = start, end
+        # add last interval
+        merged_intervals.append((current_start, current_end))
+        
+        for start, end in merged_intervals:
+            # for each of the defined intervals, grab the corresponding samples
+            buffered_rows = group[
+                (group["tSample_normalized"] >= start) &
+                (group["tSample_normalized"] <= end)]
+            # and add them to selected rows
+            selected_rows.append(buffered_rows)
+    # combine
+    cleaned_data = pd.concat(selected_rows, axis=0, ignore_index=True) 
+    return cleaned_data
+
+        
         
 
 # import data
-train = load_data("./train.csv")
+train = load_data("./train_with_saccade.csv")
 
+#mw_over_time(train, cleaned=False)
+buffer = 17000
+cleaned_df = select_mw(train, buffer)
+
+# check new distribution
+# get class imbalance
+train_mw_count = (cleaned_df["is_MW"] == 1).sum()
+train_non_mw_count = (cleaned_df["is_MW"] == 0).sum()
+print(f"Cleaned Set Ratio (MW/Non-MW): {train_mw_count / train_non_mw_count:.2f}")
+print(f"Lenth before cleaning: {len(train)}")
+print(f"length after cleaning: {len(cleaned_df)}")
+# check columns
+print("Cleaned Columns")
+print(cleaned_df.columns)
+# plot
+#mw_over_time(cleaned_df, cleaned=True)
+
+
+cleaned_df.to_csv("./train_with_saccade_balanced.csv", index=False)
+
+# OLD STRATEGY
+"""
 # plot is mw over time for each subject run pair (one plot for each subject, subplot for each run)
 #mw_over_time(train)
-
+buffer = 10000
 cleaned_df = remove_latent(train)
 # plot again
 mw_over_time(cleaned_df, cleaned=True)
@@ -127,5 +208,6 @@ print(f"Lenth before cleaning: {len(train)}")
 print(f"length after cleaning: {len(cleaned_df)}")
 
 print("saving to csv (train_balanced)")
-cleaned_df.to_csv("./train_balanced", index=False)
+cleaned_df.to_csv("./train_balanced_no_empty_runs.csv", index=False)
+"""
     
